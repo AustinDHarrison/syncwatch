dfae29278d742289c86e73622f8ba93304913d64
AustinDHarrison <117491528+AustinDHarrison@users.noreply.github.com>
2024-03-11 23:20:24 +0000

    Cinemas management. (v0.0.2)

--- scripts/sw-main.js
+++ scripts/sw-main.js
@@ -27,38 +27,150 @@ signInAnonymously(auth)
     // ...
   });
 
+export function pageLoad(){
+    const prams = new URLSearchParams(window.location.search);
+    console.log(prams.get('popup'))
 
-const prams = new URLSearchParams(window.location.search);
-console.log(prams.get('popup'))
-if(prams.get('popup') == "createCinema"){
-    var code = `
-    <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="cinemaName" placeholder="Jim's Cinema">
-        <label for="cinemaName">Cinema Name</label>
-    </div>
-    <div class="form-floating">
-        <select class="form-select" id="cinemaType">
-          <option selected>Select</option>
-          <option value="youtube">Youtube</option>
-        </select>
-    <label for="cinemaType">Cinema Media Type</label>
-    </div>`
-    createPopup("req", "Create a new cinema.", code, "Cancel", "Create");
-    document.getElementById('modal_primary').addEventListener('click', () => {
-        const cinemaName = document.getElementById('cinemaName').value;
-        const cinemaType = document.getElementById('cinemaType').value;
-        $('#popup-modal').remove();
-        $('.modal-backdrop').remove();
-        createCinemaRoom(cinemaName, cinemaType);
-    });
-    document.getElementById('modal_secondary').addEventListener('click', () => {
-        console.log("Secondary button clicked.")
-    });
+    var page = window.location.pathname;
+    if(page.endsWith("/")){
+        page = page.slice(0, -1);
+    }
+    console.log(page);
+
+    const createCinemaPages = ["/cinemas", "/cinema", ""];
+
+    if(createCinemaPages.includes(page) && prams.get('popup') == "createCinema"){
+        var code = `
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="cinemaName" placeholder="Jim's Cinema">
+            <label for="cinemaName">Cinema Name</label>
+        </div>`
+        createPopup("req", "Create a new cinema.", code, "Cancel", "Create", "/cinemas");
+        document.getElementById('modal_primary').addEventListener('click', () => {
+            const cinemaName = document.getElementById('cinemaName').value;
+            $('#popup-modal').remove();
+            $('.modal-backdrop').remove();
+            createCinemaRoom(cinemaName);
+        });
+        document.getElementById('modal_secondary').addEventListener('click', () => {
+            console.log("Secondary button clicked.")
+        });
+    }
+
+    if(createCinemaPages.includes(page) && prams.get('popup') == "cinemaConfig"){
+        const id = prams.get('id');
+        get(ref(db, `cinemas/${id}`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                const cinema = snapshot.val();
+                var code = `
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="cinemaName" placeholder="Jim's Cinema" value="${cinema["cinemaName"]}">
+                    <label for="cinemaName">Cinema Name</label>
+                </div>
+                <br>
+                    <h4 style="width:12rem;" class="text-themed">Give tickets.</h3>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control input-themed" placeholder="User's ID" aria-label="User's ID" aria-describedby="add-button" id="inviteUserInput">
+                    <button class="btn btn-themed" type="button" id="add-button">Add</button>
+                </div>
+                <div id="invites-list">
+                    <h5 style="width:11rem;" class="text-themed">Tickets given.</h5>
+
+                </div>`
+
+                createPopup("req", `<p class="text-themed">Edit your <strong>${cinema.cinemaName}</strong> cinema.</p>`, code, null, "Save", "./");
+                var itemNames;
+                try{
+                    itemNames = Object.keys(cinema["invited"]);
+                }catch{
+                    itemNames = [];
+                }
+                
+                if (itemNames.length > 0) {
+                    for (const item in itemNames) {
+                        console.log(itemNames[item]);
+                        document.getElementById("invites-list").innerHTML += `
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control input-themed" value="${itemNames[item]}" disabled>
+                        </div>`;
+                    }
+                } else {
+                    document.getElementById("invites-list").innerHTML += `
+                    <div class="input-group mb-3">
+                        <input type="text" id="no-tickets" class="form-control input-themed" value="No tickets sold." disabled>
+                    </div>`;
+                }
+                document.getElementById('modal_primary').addEventListener('click', () => {
+                    const cinemaName = document.getElementById('cinemaName').value;
+                    $('#popup-modal').remove();
+                    $('.modal-backdrop').remove();
+                    updateCinemaRoom(id, cinema, cinemaName);
+                    window.location.href = "./";
+                });
+                document.getElementById('add-button').addEventListener('click', () => {
+                    var user = document.getElementById('inviteUserInput').value;
+                    if(user){
+                        set(ref(db, `cinemas/${id}/invited/${user}`), true).then(() => {
+                            document.getElementById("invites-list").innerHTML += `<div class="input-group mb-3"><input type="text" class="form-control input-themed" value="${user}" disabled></div>`;
+                            document.getElementById('inviteUserInput').value = "";
+                            document.getElementById("no-tickets").remove();
+                        });
+                    }
+                });
+            }
+        });
+    }
+}
+    
+function updateCinemaRoom(id, cinema, cinemaName){
+    cinema["cinemaName"] = cinemaName;
+
+    set(ref(db, `cinemas/${id}/cinemaName`), cinemaName);
+
 }
 
+try{
+    document.getElementById('deleteCinema1').addEventListener('click', () => {
+        deleteCinema(0);
+    });
+    document.getElementById('deleteCinema2').addEventListener('click', () => {
+        deleteCinema(1);
+    });
+    document.getElementById('deleteCinema3').addEventListener('click', () => {
+        deleteCinema(2);
+    });
+}catch{}
 
+function deleteCinema(index){
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+            const uid = user.uid;
+            get(ref(db, `users/${user.uid}/cinemas/${index}`)).then((snapshot) => {
+                if (snapshot.exists()) {
+                    const data = snapshot.val();
+                    set(ref(db, `users/${uid}/cinemas/${index}`), null);
+                    set(ref(db, `cinemas/${data}`), null);
+                } else {
+                    console.log("No data available");
+                }
+            });
 
-function createCinemaRoom(cinemaName, cinemaType){
+        }else{
+            signInAnonymously(auth)
+            .then(() => {
+                console.log("Signed in anonymously.");
+                deleteCinema(index);
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                createPopup("error", "Error", errorMessage, "Close", "Try Again");
+              });
+        }
+        window.location.reload();
+    });
+
+}
+function createCinemaRoom(cinemaName){
     onAuthStateChanged(auth, (user) => {
         if(user){
             const uid = user.uid;
@@ -67,7 +179,6 @@ function createCinemaRoom(cinemaName, cinemaType){
             
             const room = {
                 cinemaName: cinemaName,
-                cinemaType: cinemaType,
                 owner: {
                     [uid]: true
                 }
@@ -83,7 +194,7 @@ function createCinemaRoom(cinemaName, cinemaType){
                     }else if (!data[2]){
                         set(ref(db, `users/${uid}/cinemas/2`), roomCode);
                     }else{
-                        createPopup("error", "Error", "You have reached the maximum number of cinema rooms.", "Close", "Try Again");
+                        createPopup("error", "Error", "You have reached the maximum number of cinema rooms.", "Close", "Try Again", "/cinemas");
                     }
                 } else {
                     set(ref(db, `users/${uid}/cinemas/`), [roomCode]);
@@ -116,11 +227,17 @@ export function getAccountCinemas(){
             get(ref(db, `users/${uid}/cinemas`)).then((snapshot) => {
                 if (snapshot.exists()) {
                     const cinema = snapshot.val();
-                    if(cinema[0]){
+                    if (cinema[0]) {
                         get(ref(db, `cinemas/${cinema[0]}`)).then((snapshot) => {
                             if (snapshot.exists()) {
                                 document.getElementById('cinema1Name').innerHTML = snapshot.val().cinemaName;
-                                document.getElementById('cinema1Button').setAttribute("href", `/cinemas/?id=${cinema[1]}`)  
+                                document.getElementById('cinema1Button').setAttribute("href", `/cinema/?id=${cinema[0]}`);
+                                document.getElementById('cinema1Edit').setAttribute("href", `/cinemas/?popup=cinemaConfig&id=${cinema[0]}`);
+                                var numberOfInvited = 0;
+                                snapshot.child('invited').forEach(childSnapshot => {
+                                    numberOfInvited++;
+                                });
+                                document.getElementById('cinema1InviteCount').innerHTML = `Tickets sold ${numberOfInvited}`;
                             }
                         });
                     }
@@ -128,7 +245,14 @@ export function getAccountCinemas(){
                         get(ref(db, `cinemas/${cinema[1]}`)).then((snapshot) => {
                             if (snapshot.exists()) {
                                 document.getElementById('cinema2Name').innerHTML = snapshot.val().cinemaName;
-                                document.getElementById('cinema2Button').setAttribute("href", `/cinemas/?id=${cinema[2]}`)  
+                                document.getElementById('cinema2Button').setAttribute("href", `/cinema/?id=${cinema[1]}`)  
+                                document.getElementById('cinema2Edit').setAttribute("href", `/cinemas/?popup=cinemaConfig&id=${cinema[1]}`)  
+                                var numberOfInvited = 0;
+                                snapshot.child('invited').forEach(childSnapshot => {
+                                    numberOfInvited++;
+                                });
+                                document.getElementById('cinema2InviteCount').innerHTML = `Tickets sold ${numberOfInvited}`;
+
                             }
                         });
                     }
@@ -136,7 +260,14 @@ export function getAccountCinemas(){
                         get(ref(db, `cinemas/${cinema[2]}`)).then((snapshot) => {
                             if (snapshot.exists()) {
                                 document.getElementById('cinema3Name').innerHTML = snapshot.val().cinemaName;
-                                document.getElementById('cinema3Button').setAttribute("href", `/cinemas/?id=${cinema[3]}`)  
+                                document.getElementById('cinema3Button').setAttribute("href", `/cinema/?id=${cinema[2]}`)  
+                                document.getElementById('cinema3Edit').setAttribute("href", `/cinemas/?popup=cinemaConfig&id=${cinema[2]}`)  
+                                var numberOfInvited = 0;
+                                snapshot.child('invited').forEach(childSnapshot => {
+                                    numberOfInvited++;
+                                });
+                                document.getElementById('cinema3InviteCount').innerHTML = `Tickets sold ${numberOfInvited}`;
+
                             }
                         });
                     }
